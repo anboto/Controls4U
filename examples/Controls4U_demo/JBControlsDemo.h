6e2ed83ea9f3af173d2cddc016241c633a7cbc1f
Iñaki <izabala123@gmail.com>
2022-07-23 17:09:53 +0200

    Fixes for integration with U++

--- examples/Controls4U_demo/JBControlsDemo.h
+++ examples/Controls4U_demo/JBControlsDemo.h
@@ -8,7 +8,7 @@
 
 using namespace Upp;
 
-#define LAYOUTFILE <examples/Controls4U_Demo/JBControlsDemo.lay>
+#define LAYOUTFILE <examples/Controls4U_demo/JBControlsDemo.lay>
 #include <CtrlCore/lay.h>
 
 class JBControlsDemo : public WithJBControlsDemoLayout<StaticRect> {
